a55e9361ded80d6ba629c29d24981c5ce7c8f975
qwer112211111111111111 <1481254175@qq.com>
2025-09-20 22:45:20 +0800

    Modified premakelist

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "imgui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
 
     targetdir ("bin/" .. outdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outdir .. "/%{prj.name}")
@@ -22,7 +24,11 @@ project "imgui"
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
-    filter {"system:windows", "configurations:Release"}
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
